f386114d59b887ea555cbdbb57658e3938a83c43
Takaisora <Hikui>
2019-06-01 11:48:59 +0900

    射撃分け

---
+++ Assets/Scenes/houdai/BuChangeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuChangeScript : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TBullet>().enabled = true;
+        GetComponent<FBullet>().enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKeyDown("f")) && (GetComponent<TBullet>().enabled == true)){
+            GetComponent<TBullet>().enabled = false;
+            GetComponent<FBullet>().enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+        }else if((Input.GetKeyDown("f")) && (GetComponent<FBullet>().enabled == true)){
+
+            GetComponent<TBullet>().enabled = true;
+            GetComponent<FBullet>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
