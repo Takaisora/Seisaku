edcf44bed7ff00ede11345b4426b3ecfb436022f
a <a>
2019-06-15 12:01:45 +0900

    a

---
+++ Assets/Scenes/Block/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定　相手のタグがBulletなら消す
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
