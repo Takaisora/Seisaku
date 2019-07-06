bb09d1921f3e2787c1552a82206052c3c93fa4f6
a <a>
2019-07-06 11:41:01 +0900

    201907061140 色々変更　

---
+++ Assets/Scenes/houdai/Rotation/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    //回転の中心
+    public Transform target;
+    //回転速度
+    [SerializeField]
+    public float speed = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = transform.TransformDirection(Vector3.forward);
+
+        //if (Input.GetKeyDown(KeyCode.D)){
+            transform.RotateAround(target.position, axis, speed * Time.deltaTime);
+       // }
+    }
+}
---
+++ Assets/Scenes/houdai/houdai/BuChangeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuChangeScript : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<FarBullet>().enabled = true;
+        GetComponent<CloseBullet>().enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKeyDown("f")) && (GetComponent<FarBullet>().enabled == true)){
+            GetComponent<FarBullet>().enabled = false;
+            GetComponent<CloseBullet>().enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+        }else if((Input.GetKeyDown("f")) && (GetComponent<CloseBullet>().enabled == true)){
+
+            GetComponent<FarBullet>().enabled = true;
+            GetComponent<CloseBullet>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
